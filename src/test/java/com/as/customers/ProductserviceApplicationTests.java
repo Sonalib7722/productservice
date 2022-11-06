02100865186374a0faeaca103a9236c9d7c22d6b
Sonalib7722 <115664473+Sonalib7722@users.noreply.github.com>
2022-11-06 15:26:46 +0530

    first commit

---
+++ src/test/java/com/as/customers/ProductserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.as.customers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+class ProductserviceApplicationTests {
+
+
+	void contextLoads() {
+	}
+
+}
